5a5556a26c5e4bfcac2b7cd5f0b5675426e6895f
konstantin <kosvadmail@atomlc.com.ua>
2022-06-30 22:15:26 +0200

    Добавляет оптимизацию кода, и добавляет классы при не валидной форме загрузки новой публикации

--- js/form.js
+++ js/form.js
@@ -9,10 +9,16 @@ const buttonCansel = form.querySelector('#upload-cancel');
 const effectsPreview = form.querySelectorAll('.effects__preview');
 const hashTagInput = form.querySelector('.text__hashtags');
 const commentInput = form.querySelector('.text__description');
-const pristine = new Pristine(form);
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__field-wrapper',
+  errorClass: 'img-upload__field-wrapper--error',
+  successClass: 'img-upload__field-wrapper--valid',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextTag: 'span',
+  errorTextClass: 'img-upload__field-error-text'
+});
 
-// function for close button
-const onCloseButton = () => {
+const closeModalWindow = () => {
   publicationEditor.classList.add('hidden');
   body.classList.remove('modal-open');
   fileUploader.value = '';
@@ -21,19 +27,19 @@ const onCloseButton = () => {
   hashTagInput.removeEventListener('focus', removeEscListenerOnHashTag);
   commentInput.removeEventListener('focus', removeEscListenerOnComment);
   buttonCansel.removeEventListener('click', onCloseButton);
+  buttonCansel.removeEventListener('click', onCloseEscape);
 };
-const onCloseEscape = (evt) => {
+
+// function for close button
+function onCloseButton() {
+  closeModalWindow();
+}
+function onCloseEscape (evt) {
   if(isEscape(evt)) {
-    publicationEditor.classList.add('hidden');
-    body.classList.remove('modal-open');
-    fileUploader.value = '';
-    hashTagInput.removeEventListener('focusout', addEscListenerOnHashTag);
-    commentInput.removeEventListener('focusout', addEscListenerOnComment);
-    hashTagInput.removeEventListener('focus', removeEscListenerOnHashTag);
-    commentInput.removeEventListener('focus', removeEscListenerOnComment);
-    window.removeEventListener('keydown', onCloseEscape);
+    closeModalWindow();
   }
-};
+}
+
 // Function for remove and return Event listener on ESC in modal window
 function removeEscListenerOnHashTag () {
   window.removeEventListener('keydown', onCloseEscape);
@@ -48,31 +54,38 @@ function addEscListenerOnComment () {
   window.addEventListener('keydown', onCloseEscape);
 }
 
-// Function for validation hashTags
-const hashTagValidate = (value) => {
-  const hashTagTerms = /^#[A-Za-zA-Яа-яЁё0-9]{1,20}$/;
-
-  // Checking for max 5 hashTags
+// Functions for validation hashTags
+// Checking for max 5 hashTags
+const hashTagCountValidate = (value) => {
   const hashTags = value.split(' ');
   if(hashTags.length > 5) {
     return false;
   }
+  return true;
+};
 
-  // Checking for regular expression
+// Checking for regular expression
+const hashTagTextValidate = (value) => {
+  const hashTagTerms = /^#[A-Za-zA-Яа-яЁё0-9]{1,20}$/;
+  const hashTags = value.split(' ');
+
   if (hashTags[0] !== '') {
-    let isValed = true;
+    let notValidCounter = 0;
     hashTags.forEach((hashTag) => {
-      isValed = hashTagTerms.test(hashTag);
-      if(!isValed) {
-        return false;
+      if(!hashTagTerms.test(hashTag)) {
+        notValidCounter++;
       }
     });
-    if(!isValed) {
+    if(notValidCounter > 0) {
       return false;
     }
   }
+  return true;
+};
 
-  // Checking for hashTags repetiton
+// Checking for hashTags repetiton
+const hashTagRepeatValidate = (value) => {
+  const hashTags = value.split(' ');
   if(hashTags.length >= 2) {
     for(let i = 0; i < hashTags.length - 1; i++) {
       for(let j = i + 1; j < hashTags.length; j++) {
@@ -120,8 +133,10 @@ fileUploader.addEventListener('change', () => {
   hashTagInput.addEventListener('focusout', addEscListenerOnHashTag);
   commentInput.addEventListener('focusout', addEscListenerOnComment);
 
-  pristine.addValidator(hashTagInput, hashTagValidate);
-  pristine.addValidator(commentInput, commentValidate);
+  pristine.addValidator(hashTagInput, hashTagCountValidate, 'Максимальное количество хэш-тегов 5');
+  pristine.addValidator(hashTagInput, hashTagRepeatValidate, 'Хэш-теги не должны повторяться');
+  pristine.addValidator(hashTagInput, hashTagTextValidate, 'Хэш-тег должен начинаться с # и содержать только буквы и символы');
+  pristine.addValidator(commentInput, commentValidate, 'Максимальное количество символов 140');
 
   form.addEventListener('submit', (evt) => {
     evt.preventDefault();
